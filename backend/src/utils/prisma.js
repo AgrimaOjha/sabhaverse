08ea5a19010d3092af8c746236eae75345ee81de
Agrima Ojha <agrimaojha@gmail.com>
2025-11-12 15:13:55 +0530

    Login auth

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,36 +1,3 @@
-const { PrismaClient } = require('@prisma/client');
-
-// Mock Prisma client for development without database
-const prisma = {
-  user: {
-    findUnique: () => Promise.resolve(null),
-    create: (data) => Promise.resolve({ id: '1', ...data.data }),
-    update: () => Promise.resolve({})
-  },
-  post: {
-    findMany: () => Promise.resolve([]),
-    findUnique: () => Promise.resolve(null),
-    create: () => Promise.resolve({}),
-    update: () => Promise.resolve({}),
-    delete: () => Promise.resolve({})
-  },
-  comment: {
-    create: () => Promise.resolve({}),
-    update: () => Promise.resolve({}),
-    delete: () => Promise.resolve({})
-  },
-  debate: {
-    findMany: () => Promise.resolve([]),
-    findUnique: () => Promise.resolve(null),
-    create: () => Promise.resolve({}),
-    update: () => Promise.resolve({}),
-    delete: () => Promise.resolve({}),
-    count: () => Promise.resolve(0)
-  },
-  debateReply: {
-    create: () => Promise.resolve({}),
-    deleteMany: () => Promise.resolve({})
-  }
-};
-
+const { PrismaClient } = require('../generated/prisma');
+const prisma = new PrismaClient();
 module.exports = prisma;
